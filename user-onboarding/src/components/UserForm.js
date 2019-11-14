b4b82ed1242bee62cc71b45c8df1f54f83d6ddf1
toddmurphy <toddmurphy93@gmail.com>
2019-11-14 17:33:01 -0400

    added mapPropsToValues setup to link to 'name'in Field inputs

--- user-onboarding/src/components/UserForm.js
+++ user-onboarding/src/components/UserForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { withFormik, Form, Field } from 'formik';
 import * as Yup from 'yup';
 import axios from 'axios';
@@ -37,9 +37,15 @@ const UserForm = ({ values }) => {
 }
 
 const FormikUserSignUpForm = withFormik({
-    mapPropsToValues({ }) {
+    mapPropsToValues({ name, email, password, terms }) {
         return {
-            //add properties or key values here -> name, email, password, terms of service(checkbox)
+            //add properties or key values here -> name, email, password, terms of service(checkbox) --> LINKS(maps) to 'name' property in 'Field' inputs
+            //Set each property to empty string '' for those that text inputs
+            //Set property true/false for checkbox
+            name: name || '',
+            email: email || '',
+            password: password || '',
+            terms: terms || false
         };
     }
 })(UserForm)
